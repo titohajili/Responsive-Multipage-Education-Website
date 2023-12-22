e37b05b40bb1fa89841d223b0d06157053778033
titohajili <maqatoto963@gmail.com>
2023-12-22 21:21:26 +0400

    Frequently Asked Questions

--- main.js
+++ main.js
@@ -4,3 +4,23 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 50)
 })
+
+
+ // show/hide faq answer
+
+ const faqs = document.querySelectorAll('.faq');
+
+ faqs.forEach(faq =>{
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change icon
+
+        const icon = faq.querySelector('.faq-icon i');
+        if(icon.className === 'fa-solid fa-plus'){
+            icon.className = 'fa-solid fa-minus'
+        } else{
+            icon.className = 'fa-solid fa-plus'
+        }
+    })
+ })
